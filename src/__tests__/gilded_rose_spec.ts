0439faf40aea3a68fe31216c326b77116029fa36
Kktlol <martinmalis@s.zochova.sk>
2025-09-09 10:01:23 +0200

    1hodina

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -5,7 +5,39 @@ describe("Gilded Rose", () => {
     it("should foo", () => {
         const gildedRose = new Shop([new Item("foo", 0, 0)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+        expect(items[0].name).toEqual("foo");
     });
+    
+    it("elixir should drop quality by 1", () => {
+        //arange
+        const gildedRose = new Shop([new Item("Elixir of the Mongoose", 5, 7)]);
+        //act
+        const items = gildedRose.updateQuality();
+        //assert
+        expect(items[0].quality).toEqual(6);
+    });
+
+});
+
+describe("Regular item", () => {
+    
+    it("Item should drop quality and sellin by 1", () => {
+        //arange
+        const gildedRose = new Shop([new Item("Regular item", 10, 25)]);
+        //act
+        const items = gildedRose.updateQuality();
+        //assert
+        expect(items[0].quality).toEqual(24);
+        expect(items[0].sellIn).toEqual(9);
+    });
+    it("Item should drop quality by 2 after sell in", () => {
+        //arange
+        const gildedRose = new Shop([new Item("Regular item", -1, 25)]);
+        //act
+        const items = gildedRose.updateQuality();
+        //assert
+        expect(items[0].quality).toEqual(23);
+    });
+    
 
 });
